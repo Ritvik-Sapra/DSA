0e7e9f2ddebd679f8e9a93bd086594d4b1f94446
Ritvik Sapra <sapra.ritvik@outlook.com>
2020-09-03 12:34:04 +0530

    Added iterative binary search

--- Searching/Cpp/FirstOccBinSearch.cpp
+++ Searching/Cpp/FirstOccBinSearch.cpp
@@ -6,12 +6,13 @@ NOTE: Recursive binary search is not preferred. Iterative binary search is much
 better due to less memory consumption
 
 Input 1:
-6
-1 2 2 2 3 3
-1
+6   <-- size f array
+1 2 2 2 3 3     <-- array elements
+1   <-- element to be searched for
 
 Output 1:
-0
+0   <-- Recursive binary search result
+0   <-- Iterative binary search result
 
 Input 2:
 6
@@ -19,6 +20,7 @@ Input 2:
 2
 
 Output 2:
+1
 1
 
 Input 3:
@@ -28,13 +30,14 @@ Input 3:
 
 Output 3:
 4
+4
 */
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int firstOcc(vector<int> arr, int start, int end, int x) {
+int firstOcc(vector<int> arr, int start, int end, int x) { // Using recursive Binary search
     if (start > end) // Base case
         return -1;
     int mid = ((start+end)/2);
@@ -51,6 +54,25 @@ int firstOcc(vector<int> arr, int start, int end, int x) {
     }
 }
 
+int firstOcc_iterative(vector<int> arr, int x) { // Using iterative Binary Search
+    int start = 0;
+    int end = arr.size() - 1;
+    while(start<=end) {
+        int mid = ((start+end)/2);
+        if (x < arr[mid])
+            end = mid - 1;
+        else if (x > arr[mid])
+            start = mid + 1;
+        else {
+            if((mid == 0) || (arr[mid - 1] != arr[mid]))
+                return mid;
+            else
+                end = mid - 1;
+        }
+    }
+    return -1;
+}
+
 int main() {
 	int x, size, ip;
 	vector<int> arr;
@@ -60,7 +82,8 @@ int main() {
 	    arr.push_back(ip);
 	}
 	cin>>x;
-	int index = firstOcc(arr, 0, arr.size() - 1, x);
-	cout<<index<<endl;
+	int index_recursive = firstOcc(arr, 0, arr.size() - 1, x);
+    int index_iterative = firstOcc_iterative(arr, x);
+	cout<<index_recursive<<endl<<index_iterative;
 	return 0;
 }
